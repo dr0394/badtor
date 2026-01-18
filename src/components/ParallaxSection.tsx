d65f279986805e59e91e1ba8e5c9f966bc009bd8
349websites <hallo@349websites.de>
2026-01-18 23:54:13 +0100

    Updated ParallaxSection.tsx

--- src/components/ParallaxSection.tsx
+++ src/components/ParallaxSection.tsx
@@ -33,7 +33,7 @@ const ParallaxSection = ({ onOpenQuoteModal }: ParallaxSectionProps) => {
       >
         <div className="absolute inset-0 bg-black/50 z-10" />
         <img
-          src="https://images.pexels.com/photos/4239013/pexels-photo-4239013.jpeg?auto=compress&cs=tinysrgb&w=1920"
+          src="https://images.unsplash.com/photo-1497366754035-f200968a6e72?auto=format&fit=crop&q=80&w=300"
           alt="Professionelle Reinigung"
           className="w-full h-full object-cover"
         />
