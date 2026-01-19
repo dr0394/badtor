9ca4fdb9e238f16b28e68d033b7955a36332ca90
349websites <hallo@349websites.de>
2026-01-19 10:42:45 +0100

    Updated Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -43,7 +43,7 @@ const Services = ({ onOpenQuoteModal }: ServicesProps) => {
       title: 'Büro & Unterhaltsreinigung',
       shortDesc: 'Saubere Arbeitsumgebung für produktives Arbeiten',
       fullDesc: 'Regelmäßige und zuverlässige Büroreinigung für ein gepflegtes Arbeitsumfeld. Wir sorgen für Sauberkeit und Hygiene in Ihren Geschäftsräumen.',
-      image: 'https://images.pexels.com/photos/4239122/pexels-photo-4239122.jpeg?auto=compress&cs=tinysrgb&w=800',
+      image: '/büroreinigung.png',
       cta: 'Büroreinigung anfragen',
       details: [
         'Tägliche oder wöchentliche Reinigung',
